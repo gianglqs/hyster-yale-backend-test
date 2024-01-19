ae47323ccbad8638d992eee4d8ade2ccbf41225f
gianglqs <son.giang@phoenix-software.net>
2024-01-19 10:55:47 +0700

    adjust Repository to  Executing update/delete query

--- src/main/java/com/hysteryale/repository/ProductDimensionRepository.java
+++ src/main/java/com/hysteryale/repository/ProductDimensionRepository.java
@@ -4,14 +4,17 @@ import com.hysteryale.model.ProductDimension;
 import com.hysteryale.model.Shipment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Calendar;
 import java.util.List;
 import java.util.Optional;
 
-public interface ProductDimensionRepository extends JpaRepository<ProductDimension, Long> {
+@Transactional
+public interface ProductDimensionRepository extends JpaRepository<ProductDimension, String> {
     @Query("SELECT DISTINCT a.plant FROM ProductDimension a")
     List<String> getPlants();
 
@@ -86,12 +89,15 @@ public interface ProductDimensionRepository extends JpaRepository<ProductDimensi
     @Query("SELECT DISTINCT p.truckType FROM ProductDimension p ")
     List<String> getAllTruckType();
 
+    @Modifying
     @Query("UPDATE ProductDimension p SET p.image = :imagePath WHERE p.modelCode = :modelCode")
     void updateImage(String modelCode, String imagePath);
 
+    @Modifying
     @Query("UPDATE ProductDimension p SET p.image = :imagePath, p.description = :description WHERE p.modelCode = :modelCode")
     void updateImageAndDescription(String modelCode, String imagePath, String description);
 
+    @Modifying
     @Query("UPDATE ProductDimension p SET p.description = :description WHERE p.modelCode = :modelCode")
     void updateDescription(String modelCode, String description);
 }
