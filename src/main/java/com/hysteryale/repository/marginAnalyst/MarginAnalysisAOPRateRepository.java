c78bd6df516170c53eb4b10112d3637230703080
hoangnhan <nhan.nguyen@phoenix-software.net>
2024-01-22 17:12:53 +0700

    update Calendar into LocalDate in MarginAOPRate

--- src/main/java/com/hysteryale/repository/marginAnalyst/MarginAnalysisAOPRateRepository.java
+++ src/main/java/com/hysteryale/repository/marginAnalyst/MarginAnalysisAOPRateRepository.java
@@ -4,10 +4,10 @@ import com.hysteryale.model.marginAnalyst.MarginAnalysisAOPRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Calendar;
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface MarginAnalysisAOPRateRepository extends JpaRepository<MarginAnalysisAOPRate, Integer> {
     @Query("SELECT m FROM MarginAnalysisAOPRate m WHERE m.plant = ?1 AND m.currency.currency = ?2 AND m.monthYear = ?3 AND m.durationUnit = ?4")
-    Optional<MarginAnalysisAOPRate> getMarginAnalysisAOPRate(String plant, String strCurrency, Calendar monthYear, String durationUnit);
+    Optional<MarginAnalysisAOPRate> getMarginAnalysisAOPRate(String plant, String strCurrency, LocalDate monthYear, String durationUnit);
 }
