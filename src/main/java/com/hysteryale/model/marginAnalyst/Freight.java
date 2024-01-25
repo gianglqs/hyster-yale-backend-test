a83ec8063f2f3c07250b25d40f334cf64b013e9a
hoangnhan <nhan.nguyen@phoenix-software.net>
2024-01-25 17:25:48 +0700

    mapping Freight db fields' name

--- src/main/java/com/hysteryale/model/marginAnalyst/Freight.java
+++ src/main/java/com/hysteryale/model/marginAnalyst/Freight.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
@@ -20,9 +17,12 @@ public class Freight {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "freight_sequence")
     private int id;
+
+    @Column(name = "meta_series")
     private String metaSeries;
     private double freight;
 
+    @Column(name = "month_year")
     private LocalDate monthYear;
 
     public Freight (String metaSeries, double freight, LocalDate monthYear) {
