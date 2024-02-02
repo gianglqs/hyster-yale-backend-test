5c0f5c84ef02ce7c45eda5583dcb007dd1b15cc7
hoangnhan <nhan.nguyen@phoenix-software.net>
2024-02-02 10:10:27 +0700

    fix login function with adapting email to all lowercase

--- src/main/java/com/hysteryale/repository/UserRepository.java
+++ src/main/java/com/hysteryale/repository/UserRepository.java
@@ -12,12 +12,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
-    @Query("SELECT a FROM User a WHERE a.email = ?1")
+    @Query("SELECT a FROM User a WHERE LOWER(a.email) = LOWER(?1)")
     Optional<User> getUserByEmail(String email);
-    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN TRUE ELSE FALSE END FROM User a WHERE a.email = ?1")
+    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN TRUE ELSE FALSE END FROM User a WHERE LOWER(a.email) = LOWER(?1)")
     boolean isEmailExisted(String email);
-    @Query("SELECT a FROM User a WHERE a.email = ?1 AND a.isActive = true")
+    @Query("SELECT a FROM User a WHERE LOWER(a.email) = LOWER(?1) AND a.isActive = true")
     Optional<User> getActiveUserByEmail(String email);
-    @Query("SELECT a FROM User a WHERE CONCAT(a.name, a.email) LIKE %?1%")
+    @Query("SELECT a FROM User a WHERE CONCAT(LOWER(a.name), LOWER(a.email)) LIKE CONCAT('%', LOWER(?1), '%')")
     Page<User> searchUser(String searchString, Pageable pageable);
 }
