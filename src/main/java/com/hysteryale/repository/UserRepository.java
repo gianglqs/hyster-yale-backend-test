fbe5a07a0d2f91f9624fa57af8c92aff91898de1
hoangnhan <nhan.nguyen@phoenix-software.net>
2024-02-02 10:11:21 +0700

    fix login function with adapting email to all lowercase

--- src/main/java/com/hysteryale/repository/UserRepository.java
+++ src/main/java/com/hysteryale/repository/UserRepository.java
@@ -3,12 +3,9 @@ package com.hysteryale.repository;
 import com.hysteryale.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
