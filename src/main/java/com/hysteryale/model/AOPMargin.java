90e0da37e21e248acae691126e59078b49314a82
gianglqs <son.giang@phoenix-software.net>
2024-02-05 10:51:52 +0700

    modify AOPMargin

--- src/main/java/com/hysteryale/model/AOPMargin.java
+++ src/main/java/com/hysteryale/model/AOPMargin.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
 
@@ -17,15 +16,15 @@ public class AOPMargin {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @NaturalId
-    @Column(name = "region_series_plant")
-    private String regionSeriesPlant;
     private String description;
     private double dnUSD;
     private double marginSTD;
     private int year;
     private String plant;
-    private String series;
-    private String region;
+    @Column(name = "meta_series")
+    private String metaSeries;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "region")
+    private Region region;
 
 }
