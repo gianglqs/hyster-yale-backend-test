29044b9e620d01261a665d9d1eaed200096f6b91
Khaliz <gianglqs07@gmail.com>
2024-02-22 23:42:28 +0700

    add column: modelType and success

--- src/main/java/com/hysteryale/repository/upload/FileUploadRepository.java
+++ src/main/java/com/hysteryale/repository/upload/FileUploadRepository.java
@@ -4,7 +4,12 @@ import com.hysteryale.model.upload.FileUpload;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+
 public interface FileUploadRepository extends JpaRepository<FileUpload, Integer> {
     @Query("SELECT m.fileName FROM FileUpload m WHERE m.uuid = ?1")
     String getFileNameByUUID(String uuid);
+
+    @Query("SELECT m.uploadedTime FROM FileUpload m where m.modelType = :model")
+    LocalDateTime getLatestUpdatedTimeByModelType(String modelType);
 }
