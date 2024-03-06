765088ef33f266fc0bd19672a85fda382510854e
gianglqs <son.giang@phoenix-software.net>
2024-03-06 13:53:37 +0700

    config: config resource to get uploaded files

--- src/main/java/com/hysteryale/authentication/SecurityConfig.java
+++ src/main/java/com/hysteryale/authentication/SecurityConfig.java
@@ -85,6 +85,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/oauth/login").permitAll()
                 .antMatchers("/oauth/refreshToken").permitAll()
                 .antMatchers("/images/**").permitAll()
+                .antMatchers("/uploadFiles/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/author/**").permitAll()
                 .antMatchers(HttpMethod.POST,"/users/resetPassword").permitAll()
                 // Our private endpoints
