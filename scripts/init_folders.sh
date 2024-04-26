cb7876c5fd91e287a7b825c32334fb4729c43575
gianglqs <son.giang@phoenix-software.net>
2024-04-26 18:10:47 +0700

    upgrade to upload product-dealer

--- scripts/init_folders.sh
+++ scripts/init_folders.sh
@@ -16,6 +16,9 @@ sudo mkdir -p /opt/hysteryale/uploadFiles/product
 sudo mkdir -p /opt/hysteryale/uploadFiles/exchange_rate
 sudo mkdir -p /opt/hysteryale/uploadFiles/booking_fpa
 sudo mkdir -p /opt/hysteryale/uploadFiles/residual_value
+sudo mkdir -p /opt/hysteryale/uploadFiles/dealer_product
+sudo mkdir -p /opt/hysteryale/uploadFiles/dealer
+
 
 sudo mkdir -p /opt/hysteryale/images/product/
 sudo mkdir -p /opt/hysteryale/images/part/
